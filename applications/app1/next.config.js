76f9938300c7d0b578bfc7e0e472b1e6e0509a28
Jakub Ziolkowski <ziolekjj@gmail.com>
2023-07-28 15:02:26 +0000

    fixesx

--- applications/app1/next.config.js
+++ applications/app1/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: '/app1',
 }
 
 module.exports = nextConfig
--- applications/app2/next.config.js
+++ applications/app2/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: '/app2',
 }
 
 module.exports = nextConfig
